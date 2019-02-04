177392928120c872ed9ab05e1e19f614a7ff767f
Ollin Demian <ollin.langle@ciencias.unam.mx>
2019-02-03 21:07:44 -0500

    import and export all functions needed

--- src/Node2Vec.jl
+++ src/Node2Vec.jl
@@ -1,6 +1,11 @@
 module Node2Vec.jl
-    using LightGraphs
-    using Flux
+    using SimpleWeightedGraphs, Word2Vec
 
+    export
+
+    node2vec_walk, simulate_walks, learn_embeddings
+
+    include("rw.jl")
+    using .rw
 
 end # module
