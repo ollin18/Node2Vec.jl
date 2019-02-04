884991d7195e3d6ad3e5ed03c0ab5f527367a20b
Ollin Demian <ollin.langle@ciencias.unam.mx>
2019-02-03 21:52:01 -0500

    only keep needed imports

--- src/Node2Vec.jl
+++ src/Node2Vec.jl
@@ -1,11 +1,8 @@
+__precompile__()
 module Node2Vec.jl
     using SimpleWeightedGraphs, Word2Vec
 
-    export
-
-    node2vec_walk, simulate_walks, learn_embeddings
+    export node2vec_walk, simulate_walks, learn_embeddings
 
     include("rw.jl")
-    using .rw
-
 end # module
