ac81914a2e1217ce5fc0d81490af4d3fd1e7c7f1
Ollin Langle <ollin.langle@ciencias.unam.mx>
2019-02-05 13:10:04 -0500

    export functions

--- src/Node2Vec.jl
+++ src/Node2Vec.jl
@@ -1,6 +1,39 @@
 __precompile__()
 module Node2Vec.jl
-    using SimpleWeightedGraphs, Word2Vec
+    using LightGraphs
+    using SimpleWeightedGraphs
+    using Word2Vec
+    using Pkg
+    using DelimitedFiles
+    Pkg.instantiate()
+
+    import LightGraphs:
+        _NI, AbstractGraph, AbstractEdge, AbstractEdgeIter,
+        src, dst, edgetype, nv, ne, vertices, edges, is_directed,
+        add_vertex!, add_edge!, rem_vertex!, rem_edge!,
+        has_vertex, has_edge, inneighbors, outneighbors,
+        indegree, outdegree, degree, has_self_loops, num_self_loops,
+
+        add_vertices!, adjacency_matrix, weights, connected_components, cartesian_product,
+
+        AbstractGraphFormat, loadgraph, loadgraphs, savegraph,
+        pagerank, induced_subgraph
+
+    import SimpleWeightedGraphs:
+        AbstractSimpleWeightedGraph,
+        AbstractSimpleWeightedEdge,
+        SimpleWeightedEdge,
+        SimpleWeightedGraph,
+        SimpleWeightedGraphEdge,
+        SimpleWeightedDiGraph,
+        SimpleWeightedDiGraphEdge,
+        weight,
+        weighttype,
+        get_weight,
+        WGraph,
+        WDiGraph,
+        SWGFormat
+
 
     export node2vec_walk, simulate_walks, learn_embeddings
 
