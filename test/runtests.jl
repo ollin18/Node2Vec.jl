3078978c0b2fda2b3d96445a9d458defc0cfc76a
Ollin Langle <ollin.langle@ciencias.unam.mx>
2019-02-05 16:46:05 -0500

    adding Node2Vec to req packages in test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using Word2Vec, SimpleWeightedGraphs, DelimitedFiles, LightGraphs
+using Word2Vec, SimpleWeightedGraphs, DelimitedFiles, LightGraphs, Node2Vec
 
 tred=readdlm("../data/networks/adyacencias.csv",'|')
 Nodes=readdlm("../data/networks/los_nombres.csv",',')
